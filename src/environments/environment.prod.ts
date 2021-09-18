9642a1967f9ae952c6fc60ffe68b0fe0630a7c66
Lizie Pérez <lizie.pereztorres@tcs.com>
2021-09-18 08:47:18 -0500

    Update environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https:api.openweathermap.org/data/2.5',
+  apiUrl: 'https://api.openweathermap.org/data/2.5',
   apiKey: 'bffcf4dc1d5f2e25958d9149c635fb74',
   production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https:api.openweathermap.org/data/2.5',
+  apiUrl: 'https://api.openweathermap.org/data/2.5',
   apiKey: 'bffcf4dc1d5f2e25958d9149c635fb74',
   production: false,
 };
